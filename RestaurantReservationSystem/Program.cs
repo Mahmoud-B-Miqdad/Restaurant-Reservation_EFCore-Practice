4ae2713ced8a29199d04047ed849349a09838cea
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-04-20 12:24:36 +0300

    Add initial migration and apply database schema update.

--- RestaurantReservationSystem/Program.cs
+++ RestaurantReservationSystem/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db;
+using Microsoft.EntityFrameworkCore;
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<RestaurantReservationDbContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection")));
+    })
+    .Build();
