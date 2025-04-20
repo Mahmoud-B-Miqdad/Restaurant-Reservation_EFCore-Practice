6bfdff4e9838ce9511a903a4538e0098da1eec84
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-04-20 14:11:01 +0300

    Add RestaurantService to demonstrate CRUD operations with sample data.

---
+++ RestaurantReservationSystem/EntityServices/RestaurantService.cs
@@ -0,0 +1,64 @@
+﻿using RestaurantReservation.Db.Models;
+
+public class RestaurantService
+{
+    private readonly RestaurantOperations _restaurantOperations;
+
+    public RestaurantService(RestaurantOperations restaurantOperations)
+    {
+        _restaurantOperations = restaurantOperations;
+    }
+
+    public async Task AddRestaurantAsync()
+    {
+        var restaurant = new Restaurant
+        {
+            Name = "Test Restaurant",
+            Address = "Gaza",
+            PhoneNumber = "0590000000",
+            OpeningHours = "8:00 - 16:00"
+        };
+
+        await _restaurantOperations.AddAsync(restaurant);
+    }
+
+    public async Task<List<Restaurant>> GetAllRestaurantsAsync()
+    {
+        return await _restaurantOperations.GetAllAsync();
+    }
+
+    public async Task UpdateRestaurantAsync()
+    {
+        var restaurant = new Restaurant
+        {
+            RestaurantId = 1,
+            Name = "Updated Name",
+            Address = "Updated Address",
+            PhoneNumber = "0599999999",
+            OpeningHours = "9:00 - 17:00"
+        };
+
+        await _restaurantOperations.UpdateAsync(restaurant);
+    }
+
+    public async Task DeleteRestaurantAsync()
+    {
+        int restaurantIdToDelete = 2;
+        await _restaurantOperations.DeleteAsync(restaurantIdToDelete);
+    }
+
+    public async Task ExecuteExamplesAsync()
+    {
+        await AddRestaurantAsync();
+
+        await UpdateRestaurantAsync();
+
+        await DeleteRestaurantAsync();
+
+        var all = await GetAllRestaurantsAsync();
+        foreach (var restaurant in all)
+        {
+            Console.WriteLine($"[Restaurant] {restaurant.RestaurantId} - {restaurant.Name}, {restaurant.Address}");
+        }
+    }
+}
