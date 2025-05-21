1c47f3af137410c1cf15599b7b44b2b4b27eb0e2
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-05-21 12:45:35 +0300

    Add AutoMapper profile for mapping between Restaurant entity and DTOs.

---
+++ RestaurantReservationSystem.API/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.Db.Entities;
+using RestaurantReservationSystem.API.DTOs;
+
+namespace RestaurantReservationSystem.API.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Restaurant, RestaurantResponse>();
+            CreateMap<RestaurantRequest, Restaurant>();
+        }
+    }
+}
