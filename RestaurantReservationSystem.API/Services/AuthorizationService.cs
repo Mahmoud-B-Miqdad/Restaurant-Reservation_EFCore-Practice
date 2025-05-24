c093eb1e60912347759231dab79c23321d536bc3
Mahmoud Miqdad <mahmoud.b.miqdad@gmail.com>
2025-05-24 14:43:48 +0300

    Implement basic authorization service with credential validation and JWT generation.

---
+++ RestaurantReservationSystem.API/Services/AuthorizationService.cs
@@ -0,0 +1,30 @@
+﻿namespace JwtAuthMinimalApi.Services
+{
+    public class AuthorizationService : IAuthorizationService
+    {
+
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthorizationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
+
+        public bool ValidateCredentials(string username, string password)
+        {
+            return username == "admin" && password == "1234";
+        }
+
+        public (bool Success, string? Token) Login(string username, string password)
+        {
+            if (ValidateCredentials(username, password))
+            {
+                var token = _jwtTokenGenerator.GenerateToken(username);
+                return (true, token);
+            }
+
+            return (false, null);
+        }
+    }
+
+}
---
+++ RestaurantReservationSystem.API/Services/Interfaces/IAuthorizationService.cs
@@ -0,0 +1,7 @@
+﻿namespace JwtAuthMinimalApi.Services
+{
+    public interface IAuthorizationService
+    {
+        (bool Success, string? Token) Login(string username, string password);
+    }
+}
